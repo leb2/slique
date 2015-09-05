dfd81ef994eebcad0182a78e92ff41fbf3cb63e8
Brendan Le <le.brendan@gmail.com>
2015-09-04 19:15:47 -0700

    Start conversion to SASS (finally)

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,21 @@ var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 
 gulp.task('build', function() {
-    return gulp.src('./slique.css')
+    // return gulp.src('./slique.css')
+    return gulp.src('./sass/*.scss')
 
         /* Uncomment reddit resources, e.g. background: url(%%image%%); */
         .pipe(replace(/\/\*(?:(?:.|\n)(?:(?!\*\/)))*?(.*%%.*;)(?:.|\n)*?\*\//g,'$1'))
 
         /* Remove normal urls with external resources */
         .pipe(replace(/\n.*?url\([\'\"]?[^%].*/g,''))
+
+        .pipe(sass())
+
+        .pipe(replace('@charset "UTF-8";',''))
 
         .pipe(autoprefixer())
 
@@ -20,10 +26,20 @@ gulp.task('build', function() {
 
         .pipe(minifyCss())
 
-        .pipe(rename(function(path) {
-            path.basename += '.min';
-        }))
+        .pipe(rename('./SLIQUE.css'))
+
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./stylesheets/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./sass/*.scss', ['sass']);
+});
+
 
 gulp.task('default', ['build']);
